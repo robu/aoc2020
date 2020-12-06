96ac3701edaea30285a292d5059bc2f300b9c981
Robert Buren <robert@buren.se>
2020-12-06 23:44:53 +0100

    d06p1 cleanup

--- d06/part1.py
+++ d06/part1.py
@@ -6,24 +6,15 @@ def read_input():
         lines = a_file.read().splitlines()
     return lines
 
-
 if __name__ == '__main__':
     answers = set() # one set of answers per group
     groups = [] # list of all groups' sets of answers
-    count = 0
     for line in read_input():
         if len(line) == 0:
-            count += len(answers)
             groups.append(answers)
-            print("{0} ({1}): {2}".format(len(answers), count, answers))
             answers = set()
         else:
-            for c in line:
-                answers.add(c)
+            for c in line: answers.add(c)
     if (len(answers) > 0): groups.append(answers)
-
+    count = reduce(lambda acc, elem: acc+elem, map(lambda x: len(x), groups), 0)
     print(count)
-
-    # count the sum in a different way, to confirm. however, the results are the same
-    c2 = reduce(lambda acc, elem: acc+elem, map(lambda x: len(x), groups), 0)
-    print(c2)
