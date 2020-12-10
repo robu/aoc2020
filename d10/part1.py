9921ab3cbc9c0a70b2cf25e9a66cc35b1d91baf1
Robert Buren <robert@buren.se>
2020-12-10 14:35:18 +0100

    d10p1 done

---
+++ d10/part1.py
@@ -0,0 +1,25 @@
+def read_input():
+    nums = []
+    with open('input.txt') as a_file:
+        nums = [int(a) for a in a_file.read().splitlines()]
+    return nums
+
+if __name__ == '__main__':
+    nums = read_input()
+    target = max(nums) + 3
+    base = 0
+    max_delta = 3
+    prev_jolt = base
+    delta1_count = 0
+    delta3_count = 0
+    for n in sorted(nums):
+        delta = n - prev_jolt
+        if delta <= max_delta and n + max_delta <= target:
+            if delta == 1: delta1_count += 1
+            if delta == 3: delta3_count += 1
+        prev_jolt = n
+    delta3_count += 1 # add one for jolt to device
+
+    print("d1: {}, d3: {}".format(delta1_count, delta3_count))
+    print("product: {}".format(delta1_count * delta3_count))
+    # print(sorted(nums))
